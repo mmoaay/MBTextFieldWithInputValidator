0f2ac3a4bc9c059b12e21ca9522f69f08cbeb37b
Perry <perry@PerrydeMac-mini.local>
2015-07-06 18:14:28 +0800

    添加多策略匹配

--- MBTextFieldWithInputValidator/MBTextFieldWithInputValidator/MBTextFieldWithInputValidator.swift
+++ MBTextFieldWithInputValidator/MBTextFieldWithInputValidator/MBTextFieldWithInputValidator.swift
@@ -9,20 +9,20 @@
 import UIKit
 
 class MBTextFieldWithInputValidator: UITextField {
-    @IBOutlet var inputValidator:MBInputValidator?
+    @IBOutlet var inputValidators:[MBInputValidator]!
     
     func validate(inputName:String, shouldAlert:Bool) -> MBInputValidator.ErrorDesc? {
-        
-        let error = self.inputValidator!.validateInput(self)
-        if nil != error {
-            let errorReason = (error?.leading)!+inputName+(error?.trailing)!
-            if true == shouldAlert {
-                self.showAlertMessage((error?.title)!, message: errorReason)
+        for validator:MBInputValidator in self.inputValidators {
+            let error = validator.validateInput(self)
+            if nil != error {
+                let errorReason = (error?.leading)!+inputName+(error?.trailing)!
+                if true == shouldAlert {
+                    self.showAlertMessage((error?.title)!, message: errorReason)
+                }
+                return error
             }
-            return error
-        } else {
-            return nil
         }
+        return nil
     }
     
     private func showAlertMessage(title:String ,message:String) {
